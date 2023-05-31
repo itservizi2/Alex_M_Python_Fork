da7ba359368d369964ddd0279c5a59946fc54849
mtricolici <mtricolici@inthergroup.com>
2023-05-31 18:17:47 +0300

    Initial commit

---
+++ lesson_2_homework/ex2.py
@@ -0,0 +1,5 @@
+# Completeaza codul in asa mod ca rezultatul sa fie 18
+
+a = ___
+b = 6
+print(a * b)
---
+++ lesson_2_homework/ex5.py
@@ -0,0 +1,5 @@
+# Creaţi o constantă cu numele CONST_MATEMATICA şi atribuiţi-i valoarea 5.
+
+# Creaţi după aceasta o variabila `current_nr` şi atribuiţi-i valoarea 1.
+# Afişaţi după aceasta la ecran valorile 5, 15, 20, 25 folosind numai
+# constanta CONST_MATEMATICA şi variabila current_nr.
