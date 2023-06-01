2aadfdfdb6eb899a46460a1495a592b74dbf9dcd
itservizi2 <itservizi.helpdesk@gmail.com>
2023-06-01 17:45:05 +0300

    lesson 2 updated with 1. added minutes for being displayed 2. added second display method using f-string

--- lesson_2_homework/extra6.py
+++ lesson_2_homework/extra6.py
@@ -1,7 +1,10 @@
 ## Exercițiul 6: Minute în ore și minute
 #Scrie un program care preia numărul de minute ca intrare și îl convertește în ore și minute. Afișează orele și minutele convertite.
 
-minute = int(input("insert nr of minutes : "))
-ore = minute / 60
-print(str(minute) + " minute = " + str(ore) + " ore")
+minute_in_total = int(input("insert nr of minutes : "))
+ore = minute_in_total // 60
+minute = minute_in_total % 60
 
+print(str(minute_in_total) + " minute = " + str(ore) + " ore " + str(minute) + " minute ")
+
+print(f"{minute_in_total} minute sunt echivalente cu {ore} ore și {minute} minute.")
