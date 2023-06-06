5beed6d04229dd8e61bba21bbdab4b38a74d3c4a
mtricolici <mtricolici@inthergroup.com>
2023-06-06 12:17:33 +0300

    Added ex7

---
+++ lesson_4_homework/ex7.py
@@ -0,0 +1,10 @@
+"""
+Scrieți un program pentru a citi temperatura de la utilizator și pentru a afișa un mesaj adecvat în funcție de starea temperaturii de mai jos.
+Temp -40-(-10) atunci 'Vremea extrem de rece'
+Temp -10-0 atunci 'Vremea foarte rece'
+Temp 0-10 atunci 'Vreme rece'
+Temp 10-20 atunci 'Vreme normala'
+Temp 20-30 atunci 'Vreme calda'
+Temp 30-40 atunci 'Este foarte cald'
+Temp >=40 atunci 'Este extrem de cald'
+"""
