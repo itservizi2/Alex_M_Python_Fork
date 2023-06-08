efe9d07a69ab58fe53dec18e7072a6e3611a5225
itservizi2 <itservizi.helpdesk@gmail.com>
2023-06-08 18:59:13 +0300

    lesson 5 homework updated ex 2

--- lesson_5_homework/ex2.py
+++ lesson_5_homework/ex2.py
@@ -9,6 +9,6 @@ print(list_2)
 
 #metoda2 crearea altei liste
 list_2a = [5, 8, 2, 10, 4, 3, 1, 9]
-list_2a.sort(reverse=True)
-print(list_2a)
+list_2a_sortata = sorted(list_2a, reverse=True)
+print(list_2a_sortata)
 
